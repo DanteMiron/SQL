2811406f5a20853f464138c1ccb35fc3deeecae3
DanteMiron <dantemiron@hotmail.com>
2024-08-20 15:35:44 -0300

    terminando unidad 7

--- clases/unidad-siete.sql
+++ clases/unidad-siete.sql
@@ -21,3 +21,35 @@ select a.name as country, b.name as continent from country a
 inner join continent b on a.continent = b.code
 order by a.name asc;
 
+-- tareas 
+
+-- ¿Cuál es el idioma (y código del idioma) oficial más hablado por diferentes países en Europa?
+
+select * from countrylanguage where isofficial = true;
+
+select * from country;
+
+select * from continent;
+
+Select * from "language";
+
+select count(*), a.languagecode, a."language"  from countrylanguage a
+inner join country b on a.countrycode = b.code
+inner join continent c on b.continent = c.code
+where (a.isofficial = true) AND (c."name" like 'Europe')
+group by a.languagecode, a."language"
+order by count(*) DESC
+limit 1;
+
+
+
+
+
+
+
+-- Listado de todos los países cuyo idioma oficial es el más hablado de Europa 
+-- (no hacer subquery, tomar el código anterior)
+
+select * from country a
+inner join countrylanguage b on a.code = b.countrycode
+where b.isofficial = true AND a.continent = 5 AND b.languagecode = 135;
